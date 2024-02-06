2b51517fa2680c46824fd5a8747597d468922f74
kirill111111111 <155085103+kirill111111111@users.noreply.github.com>
2024-02-07 00:06:36 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите значение M:");
+        int m = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите значение N:");
+        int n = int.Parse(Console.ReadLine());
+
+        PrintNaturalNumbersInRange(m, n);
+
+        Console.ReadLine(); 
+    }
+
+    static void PrintNaturalNumbersInRange(int m, int n)
+    {
+        if (m <= n)
+        {
+            Console.Write($"{m} ");
+            PrintNaturalNumbersInRange(m + 1, n);
+        }
+    }
+}
